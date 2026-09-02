d4181134c21dc70667e23b018217cf62fced09de
shivanathbabu kodimyala <shivanathbabu.k@websynergies.com>
2026-09-02 12:32:42 +0530

    git

--- main.tf
+++ main.tf
@@ -142,19 +142,19 @@ resource "aws_route" "database" {
 
 resource "aws_route_table_association" "public_route_table_association" {
   count = length(var.public_subnet)
-  subnet_id = aws_subnet.public_subnet[count.index]
+  subnet_id = aws_subnet.public_subnet[count.index].id
   route_table_id = aws_route_table.public_route.id
 
 }
 
 resource "aws_route_table_association" "private_route_table_association" {
   count = length(var.private_subnet)
-  subnet_id = aws_subnet.private_subnet[count.index]
+  subnet_id = aws_subnet.private_subnet[count.index].id  
   route_table_id = aws_route_table.private_route.id
 }
 
 resource "aws_route_table_association" "database_route_table_association" {
-  count = length(var.private_subnet)
-  subnet_id = aws_subnet.private_subnet[count.index]
+  count = length(var.database_subnet)
+  subnet_id = aws_subnet.database_subnet[count.index].id 
   route_table_id = aws_route_table.database_route.id
 }
