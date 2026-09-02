2229b2fe88e65c3dca856f2997f7ec3ba33be131
shivanathbabu kodimyala <shivanathbabu.k@websynergies.com>
2026-09-02 12:24:17 +0530

    git

--- peering.tf
+++ peering.tf
@@ -16,7 +16,7 @@ resource "aws_vpc_peering_connection" "foo" {
 resource "aws_route" "public_subnet_default_vpc" {
   count = var.is_peering_required ? 1 : 0
   route_table_id = aws_route_table.public_route.id
-  destination_cidr_block = data.aws_subnets.default.cidr_block
+  destination_cidr_block = data.aws_vpc.default.cidr_block
   vpc_peering_connection_id = aws_vpc_peering_connection.foo[count.index].id
 }
 
@@ -30,7 +30,7 @@ resource "aws_route" "private_subnet_default_vpc" {
 resource "aws_route" "database_subnet_default_vpc" {
   count = var.is_peering_required ? 1 : 0
   route_table_id = aws_route_table.public_route.id
-  destination_cidr_block = data.aws_subnets.default.cidr_block  
+  destination_cidr_block = data.aws_vpc.default.cidr_block
   vpc_peering_connection_id = aws_vpc_peering_connection.foo[count.index].id
 
 }
